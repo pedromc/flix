1d26c61e7987f0f6e3a14178567309e244459177
pedromc <pedromc@gmail.com>
2025-11-14 18:27:52 -0500

    Adding all controllers for movies

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -13,10 +13,30 @@ class MoviesController < ApplicationController
   
   def update
     @movie = Movie.find(params[:id])
-    movie_params =
-      params.require(:movie).
-        permit(:title, :description, :rating, :released_on, :total_gross)
     @movie.update(movie_params)
     redirect_to @movie
   end
+  
+  def new
+    @movie = Movie.new
+   end
+   
+   def create
+    @movie = Movie.new(movie_params)
+    @movie.save
+    redirect_to @movie
+   end
+   
+   def destroy
+    @movie = Movie.find(params[:id])
+    @movie.destroy
+    redirect_to movies_url, status: :see_other
+  end     
+
+private
+  def movie_params
+    params.require(:movie).
+      permit(:title, :description, :rating, :released_on, :total_gross)
+  end
+
 end
